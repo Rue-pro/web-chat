61a3bb2fd0018008ee92c654a645b8f727642eb0
Ruelina <ruesullivanpro@gmail.com>
2022-03-17 21:35:33 +0400

    feat: add PassportModule and JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secretOrPrivateKey: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: 3600,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService],
   exports: [AuthService],
 })
