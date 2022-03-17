4f919a5c7ef646dfcaa3096c5eaaac7007ab169e
Ruelina <ruesullivanpro@gmail.com>
2022-03-17 18:25:56 +0400

    feat: add fake seed dataand other things

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { setupSwagger } from './api-docs.swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  app.useGlobalPipes(new ValidationPipe());
 
   setupSwagger(app);
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,25 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
+import { IsNotEmpty, IsOptional, MinLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   name: string;
 
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   email: string;
 
+  @IsNotEmpty()
   @ApiProperty()
   phone: string;
 
+  @IsNotEmpty()
+  @MinLength(12)
   @ApiProperty()
   password: string;
 
+  @IsOptional()
   @ApiProperty({ required: false })
   avatar?: string;
 
-  @ApiProperty({ required: false })
-  avatarId?: string;
-
+  @IsOptional()
   @ApiProperty({ required: false })
   role?: Role;
 }
