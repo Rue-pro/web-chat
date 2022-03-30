8fb2748a83d3e35cc735ad281018b387c3e28bab
Alina Zinovyeva <alina.zinovyeva@bravedevelopers.com>
2022-03-30 11:39:53 +0400

    feat: add getting user id from token to get messages

--- apps/backend/src/messages/messages.service.ts
+++ apps/backend/src/messages/messages.service.ts
@@ -16,12 +16,12 @@ export class MessagesService {
   async getUserFromSocket(socket: Socket) {
     console.log('GET_USER_FROM_SOCKET');
     const cookie = socket.handshake.headers.cookie;
-    console.log('COOKIE', cookie);
-    const { access_token: authenticationToken } = parse(cookie);
-    console.log('AUTHENTICATION_TOKEN', authenticationToken);
+    const { access_token } = parse(cookie);
+    console.log('ACCESS_TOKEN', access_token);
     const user = await this.authService.getUserFromAuthenticationToken(
-      authenticationToken,
+      access_token,
     );
+    console.log('USER', user);
     if (!user) {
       throw new WsException('Invalid credentials.');
     }
@@ -32,9 +32,8 @@ export class MessagesService {
     return this.prisma.message.create({ data: createMessageDto });
   }
 
-  async getAllMessages(id: any) {
+  async getAllMessages(id: string) {
     console.log('GET_ALL_MESSAGES');
-    const authorId = 'cl10bf6sg000497l4lfivj7k7';
-    return this.prisma.message.findMany({ where: { authorId: authorId } });
+    return this.prisma.message.findMany({ where: { authorId: id } });
   }
 }
