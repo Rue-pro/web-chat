58c08db39f300e697e6256f980fc87cdc0981136
Ruelina <ruesullivanpro@gmail.com>
2022-04-07 13:02:46 +0400

    refactor: remove getUserFromSocket to auth

--- apps/backend/src/messages/messages.service.ts
+++ apps/backend/src/messages/messages.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Brackets, Repository } from 'typeorm';
 
-import { Socket } from 'socket.io';
-import { WsException } from '@nestjs/websockets';
-import { parse } from 'cookie';
-
 import { AuthService } from 'src/auth/auth.service';
 import { MessageEntity } from './entity';
 import { CreateMessageDto } from './dto';
@@ -17,21 +13,6 @@ export class MessagesService {
     @InjectRepository(MessageEntity)
     private readonly messageRepository: Repository<MessageEntity>,
   ) {}
-
-  async getUserFromSocket(socket: Socket) {
-    console.log('GET_USER_FROM_SOCKET');
-    const cookie = socket.handshake.headers.cookie;
-    const { access_token } = parse(cookie);
-    console.log('ACCESS_TOKEN', access_token);
-    const user = await this.authService.getUserFromAuthenticationToken(
-      access_token,
-    );
-    console.log('USER', user);
-    if (!user) {
-      throw new WsException('Invalid credentials.');
-    }
-    return user;
-  }
 
   saveMessage(createMessageDto: CreateMessageDto) {
     return this.messageRepository.save({
