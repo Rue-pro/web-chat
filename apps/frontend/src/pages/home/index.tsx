3977eb4e5c603380c7c978e10ae61219391fabd4
Alina Zinovyeva <alina.zinovyeva@bravedevelopers.com>
2022-04-11 14:06:19 +0400

    feat: add closing dialog on escape button press

--- apps/frontend/src/pages/home/index.tsx
+++ apps/frontend/src/pages/home/index.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
-import { Box } from '@mui/material'
 
 import Chat from 'widgets/chat'
-import Template from 'shared/ui/template'
+import { ChatTemplate } from 'shared/ui/template'
 import { Profile } from 'entities/profile'
 
 const HomePage: React.FC = () => {
-  return (
-    <>
-      <Template
-        title="Чат"
-        aside={<Profile />}
-        main={
-          <>
-            <div>Protected page</div>
-            <Chat />
-          </>
-        }
-      />
-    </>
-  )
+  return <ChatTemplate title="Чат" aside={<Profile />} main={<Chat />} />
 }
 
 export default HomePage
