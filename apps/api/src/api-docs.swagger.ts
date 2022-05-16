e3b49efdfdcb15530cad3ef16e7894180104cf61
Alina Zinovyeva <alina.zinovyeva@bravedevelopers.com>
2022-05-16 11:07:35 +0400

    misc: add web deploy to heroku

--- apps/api/src/api-docs.swagger.ts
+++ apps/api/src/api-docs.swagger.ts
@@ -9,5 +9,5 @@ export function setupSwagger(app: INestApplication) {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 }
